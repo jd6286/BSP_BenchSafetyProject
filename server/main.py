4e7636b6aef5e73edf2176bb00a89c04bba705b2
Kwon Ohjoon <koj1997@gmail.com>
2024-06-26 19:57:51 +0900

    쓰레드 클래스를 thread.py로 분리

--- server/main.py
+++ server/main.py
@@ -1,79 +1,29 @@
 """
 서버 메인 파일
 """
-from configparser import ConfigParser
-from threading import Thread
-from queue import Queue
-
-from utils.communication import (
-    ImageDisplayThread,
-    ImageReceiverThread, 
-    MessageSender, 
-    create_client_thread)
-from utils.model import PersonDetector, PoseClassifier, PoseEstimator
-
-
-def control_threads(task: str, threads: list[Thread]):
-    """
-    쓰레드를 제어하는 함수
-    """
-    if task == 'start':
-        for thread in threads:
-            thread.daemon = True
-            thread.start()
-    elif task == 'stop':
-        for thread in threads:
-            thread.stop()
-    elif task == 'join':
-        for thread in threads:
-            thread.join()
+from utils.communication import MessageSender, init_communication
+from utils.thread import ImageReceiveThread, InferenceThread
 
 
 if __name__ == '__main__':
-    # Config 파일 읽기
-    config = ConfigParser()
-    config.read('config.ini')
+    # 클라이언트와의 통신 초기화
+    thread_dict = {}
+    init_communication(thread_dict)
 
-    client1_image_port = int(config['client1']['img_port'])
-    client1_message_port = int(config['client1']['msg_port'])
-    # client2_image_port = int(config['client2']['img_port'])
-    # client2_message_port = int(config['client2']['msg_port'])
+    # 쓰레드 객체
+    client1_image_receive_thread: ImageReceiveThread = thread_dict['Client1 Image'][0]
+    client1_inference_thread: InferenceThread = thread_dict['Client1 Image'][1]
+    client1_message_sender: MessageSender = thread_dict['Client1 Message']
 
-    # 연결 정보
-    server_ip = '0.0.0.0'
-    ports = [
-        (client1_image_port, 'Client1 Image'), 
-        (client1_message_port, 'Client1 Message'), 
-        # (client2_image_port, 'Client2 Image'), 
-        # (client2_message_port, 'Client2 Message')
-    ]
-
-    # 통신 쓰레드 생성
-    client_threads = {}
-    create_client_thread(server_ip, ports, client_threads)
-    
-    client1_image_queue, client1_image_thread, client1_display_thread = client_threads['Client1 Image']
-    client1_message_sender = client_threads['Client1 Message']
-    # client2_image_queue, client2_image_thread, client2_display_thread = client_sockets['Client2 Image']
-    # client2_message_sender = client_sockets['Client2 Message']
-
-    thread_list = [
-        client1_image_thread, 
-        client1_display_thread, 
-        # client2_image_thread, 
-        # client2_display_thread
-    ]
-
-    # 쓰레드 시작
-    control_threads('start', thread_list)
-
+    # 무한 루프
     try:
         while True:
             pass
     except Exception as e:
         print(e)
     finally:
-        control_threads('stop', thread_list)
+        client1_image_receive_thread.stop()
+        client1_inference_thread.stop()
 
-    # 쓰레드 종료 대기
-    control_threads('join', thread_list)
+    client1_image_receive_thread.join()
+    client1_inference_thread.join()
--- server/utils/communication.py
+++ server/utils/communication.py
@@ -1,15 +1,18 @@
 """
 통신을 위한 모듈
 """
+if __name__ == '__main__':
+    import sys
+    print('This script cannot be run independently.')
+    sys.exit(1)
+
+import configparser
 import threading
-import traceback
 import socket
-import struct
 from typing import Any
 from queue import Queue
 
-import cv2
-import numpy as np
+from utils.thread import ImageReceiveThread, InferenceThread
 
 
 class MessageSender:
@@ -35,87 +38,6 @@ class MessageSender:
         self._socket.sendall(message.encode('utf-8'))
 
 
-class ImageReceiverThread(threading.Thread):
-    """
-    이미지 수신 쓰레드
-
-    Args:
-        client_socket (socket.socket): 클라이언트 소켓
-        image_queue (Queue): 수신한 이미지를 저장할 큐
-    """
-    def __init__(self, client_socket: socket.socket, image_queue: Queue):
-        super().__init__()
-        self._socket = client_socket
-        self._socket.settimeout(10.0)
-        self._queue = image_queue
-        self._running = True
-    
-    def __del__(self):
-        self._socket.close()
-        cv2.destroyAllWindows()
-    
-    def run(self):
-        # 이미지 수신
-        try:
-            while self._running:
-                # 이미지 크기 수신
-                img_size_data = self._socket.recv(4)
-                if not img_size_data:
-                    break
-                img_size = struct.unpack(">L", img_size_data)[0]
-
-                # 이미지 데이터 수신
-                img_data = b""
-                while len(img_data) < img_size:
-                    data = self._socket.recv(img_size - len(img_data))
-                    if not data:
-                        break
-                    img_data += data
-                
-                # 수신한 데이터를 이미지로 변환하여 큐에 추가
-                img_array = np.frombuffer(img_data, dtype=np.uint8)
-                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                self._queue.put(img)
-        except Exception as e:
-            traceback.print_exc()
-            self._running = False
-
-    def stop(self):
-        self._running = False
-
-
-class ImageDisplayThread(threading.Thread):
-    """
-    이미지 표시 쓰레드
-
-    Args:
-        image_queue (Queue): 이미지가 저장된 큐
-    """
-    def __init__(self, image_queue: Queue):
-        super().__init__()
-        self._queue = image_queue
-        self._running = True
-
-    def run(self):
-        cv2.namedWindow('Press ESC to exit', cv2.WINDOW_NORMAL)
-
-        try:
-            while self._running:
-                if not self._queue.empty():
-                    img = self._queue.get()
-                    cv2.imshow('Press ESC to exit', img)
-                if cv2.waitKey(1) == 27:
-                    break
-        except Exception as e:
-            traceback.print_exc()
-            self._running = False
-        finally:
-            cv2.destroyAllWindows()
-    
-    def stop(self):
-        self._running = False
-
-
 def accept_connection(
         server_socket: socket.socket, 
         key: str, 
@@ -135,29 +57,39 @@ def accept_connection(
     # Key에 맞는 객체 생성 후 딕셔너리에 저장
     if 'Image' in key:
         image_queue = Queue()
-        image_receiver_thread = ImageReceiverThread(client_socket, image_queue)
-        image_display_thread = ImageDisplayThread(image_queue)
-        return_dict[key] = (image_queue, image_receiver_thread, image_display_thread)
+        image_receive_thread = ImageReceiveThread(client_socket, image_queue)
+        inference_thread = InferenceThread(image_queue)
+        return_dict[key] = (image_receive_thread, inference_thread)
     elif 'Message' in key:
         return_dict[key] = MessageSender(client_socket)
     else:
         raise ValueError(f'Invalid key: {key}')
 
 
-def create_client_thread(
-        server_ip: str, 
-        ports: dict[str, int], 
-        return_dict: dict[str, Any]):
+def init_communication(return_dict: dict):
     """
-    클라이언트와 통신하는 쓰레드 생성하는 함수
-
-    Args:
-        server_ip (str): 서버 IP 주소
-        ports (list): 포트와 키가 담긴 튜플들의 리스트
-        return_dict (dict): 쓰레드 객체를 저장할 딕셔너리
+    통신 초기화 함수
     """
-    threads = []
+    # Config 파일 읽기
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
+    # 연결 정보
+    server_ip = '0.0.0.0'
+    client1_image_port = int(config['client1']['img_port'])
+    client1_message_port = int(config['client1']['msg_port'])
+    # client2_image_port = int(config['client2']['img_port'])
+    # client2_message_port = int(config['client2']['msg_port'])
+    
+    ports = [
+        (client1_image_port, 'Client1 Image'), 
+        (client1_message_port, 'Client1 Message'), 
+        # (client2_image_port, 'Client2 Image'), 
+        # (client2_message_port, 'Client2 Message')
+    ]
+
+    # 통신 쓰레드 생성
+    temp_threads = []
     for port, key in ports:
         # 서버 소켓 생성
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -171,7 +103,18 @@ def create_client_thread(
                                   args=(server_socket, key, return_dict))
         thread.daemon = True
         thread.start()
-        threads.append(thread)
+        temp_threads.append(thread)
     
-    for thread in threads:
+    for thread in temp_threads:
         thread.join()
+
+    # 쓰레드 실행
+    for key, threads in return_dict.items():
+        if 'Image' in key:
+            for thread in threads:
+                thread.daemon = True
+                thread.start()
+        elif 'Message' in key:
+            pass
+        else:
+            raise ValueError(f'Invalid key: {key}')
