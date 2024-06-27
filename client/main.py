607446e9309a353a5db653fae09a15daeb79fef4
Kwon Ohjoon <koj1997@gmail.com>
2024-06-27 13:31:33 +0900

    Client 하드웨어 제어 기능 추가

--- client/main.py
+++ client/main.py
@@ -2,7 +2,11 @@
 클라이언트 메인 파일
 """
 from utils.communication import init_communication
+from utils.hardware import PiHardware
 from utils.thread import ImageSendThread, MessageReceiveThread
+
+
+main_running = True
 
 
 # 통신 종료 함수
@@ -15,10 +19,14 @@ def stop_communication(image_sender: ImageSendThread,
         image_sender (ImageSendThread): 이미지 송신 쓰레드
         message_receiver (MessageReceiveThread): 메시지 수신 쓰레드
     """
+    global main_running
+
     if image_sender is not None and image_sender.is_alive():
         image_sender.stop()
     if message_receiver is not None and message_receiver.is_alive():
         message_receiver.stop()
+    
+    main_running = False
 
 
 if __name__ == "__main__":
@@ -27,11 +35,16 @@ if __name__ == "__main__":
     image_sender: ImageSendThread = threads[0]
     message_receiver: MessageReceiveThread = threads[1]
 
+    # 하드웨어 초기화
+    hardware = PiHardware()
+
     # 메시지 콜백 등록
     message_receiver.add_callback('exit', lambda: stop_communication(image_sender, message_receiver))
+    message_receiver.add_callback('buzzer on', lambda: hardware.buzzer_on(1))
+    message_receiver.add_callback('buzzer off', lambda: hardware.buzzer_off())
 
     try:
-        while True:
+        while main_running:
             pass
     except Exception as e:
         print(e)
--- client/utils/hardware.py
+++ client/utils/hardware.py
@@ -1,3 +1,47 @@
 """
 라즈베리 파이 하드웨어 제어 모듈
 """
+import time
+import threading
+
+import RPi.GPIO as GPIO
+
+
+class PiHardware:
+    # GPIO 핀 번호
+    BUZZER_PIN = 18
+    def __init__(self):
+        # GPIO 초기화
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.BUZZER_PIN, GPIO.OUT)
+
+        self.buzzer_pwm = GPIO.PWM(self.BUZZER_PIN, 500)
+        self.buzzer_state = False
+        self.buzzer_thread = None
+
+        # 초기 상태 설정
+        self.buzzer_pwm.stop()
+        GPIO.output(self.BUZZER_PIN, GPIO.LOW)
+
+    def __del__(self):
+        GPIO.cleanup()
+
+    def _do_buzz(self, duration: float):
+        while self.buzzer_state:
+            self.buzzer_pwm.start(50)
+            time.sleep(duration)
+
+            self.buzzer_pwm.stop()
+            GPIO.output(self.BUZZER_PIN, GPIO.LOW)
+            time.sleep(duration)
+    
+    def buzzer_on(self, duration: float):
+        if self.buzzer_state == False:
+            self.buzzer_state = True
+            self.buzzer_thread = threading.Thread(target=self._do_buzz, args=(duration,))
+            self.buzzer_thread.start()
+    
+    def buzzer_off(self):
+        self.buzzer_state = False
+        self.buzzer_pwm.stop()
+        GPIO.output(self.BUZZER_PIN, GPIO.LOW)   
