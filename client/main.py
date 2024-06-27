4da864fbdb6b85c1d568f373e550311905de033a
Kwon Ohjoon <koj1997@gmail.com>
2024-06-27 10:25:05 +0900

    Client 통신 기능 추가

--- client/main.py
+++ client/main.py
@@ -1,3 +1,23 @@
 """
 클라이언트 메인 파일
 """
+from utils.communication import init_communication
+from utils.thread import ImageSendThread, MessageReceiveThread
+
+
+if __name__ == "__main__":
+    threads = init_communication()
+    image_send_thread: ImageSendThread = threads[0]
+    message_receive_thread: MessageReceiveThread = threads[1]
+
+    try:
+        while True:
+            pass
+    except Exception as e:
+        print(e)
+    finally:
+        image_send_thread.stop()
+        message_receive_thread.stop()
+
+    image_send_thread.join()
+    message_receive_thread.join()
--- client/utils/communication.py
+++ client/utils/communication.py
@@ -2,105 +2,55 @@
 통신을 위한 모듈
 """
 import sys
-import struct
-import threading
+import configparser
+import socket
 import traceback
 
-import cv2
+from utils.thread import ImageSendThread, MessageReceiveThread
 
 
-class ImageSender(threading.Thread):
+def init_communication():
     """
-    서버로 이미지를 전송하는 쓰레드
+    통신 초기화 함수
 
-    Args:
-        image_socket (socket.socket): 이미지 송신용 소켓
+    Returns:
+        ImageSendThread: 이미지 송신 쓰레드
+        MessageReceiveThread: 메시지 수신 쓰레드
     """
-    def __init__(self, image_socket):
-        super().__init__()
-        self._socket = image_socket             # 이미지 송신용 소켓
-        self._camera = cv2.VideoCapture(0)      # 웹캠
-        self._running = True                    # 쓰레드 실행 여부
+    # Config 파일 읽기
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-        if not self._camera.isOpened():
-            print('Error: Could not open webcam.')
-            sys.exit()
+    # 서버 IP, 포트 설정
+    server_ip = config['server']['ip']
+    image_port =  int(config['server']['img_port'])
+    message_port = int(config['server']['msg_port'])
+
+    # 소켓 생성
+    image_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    message_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 서버에 연결
+    try:
+        print(f'Connecting to the server {server_ip}:{image_port}...')
+        image_socket.connect((server_ip, image_port))
+        print(f'Connecting to the server {server_ip}:{message_port}...')
+        message_socket.connect((server_ip, message_port))
+        print('Connected to the server.')
+    except ConnectionRefusedError:
+        print('Connection is refused by the server.')
+        return
+    except Exception:
+        traceback.print_exc()
+        return
     
-    def __del__(self):
-        self._socket.close()
-        if self._camera is not None:
-            self._camera.release()
+    # 쓰레드 실행
+    image_send_thread = ImageSendThread(image_socket)
+    image_send_thread.daemon = True
+    image_send_thread.start()
 
-    def run(self):
-        try:
-            while self._running:
-                # 웹캠에서 이미지를 읽어옴
-                ret, frame = self._camera.read()
-                if not ret:
-                    print('Error: Could not read frame from webcam.')
-                    break
+    message_receive_thread = MessageReceiveThread(message_socket)
+    message_receive_thread.daemon = True
+    message_receive_thread.start()
 
-                # 이미지를 JPEG 포맷으로 인코딩 후 바이트로 변환
-                _, img_encoded = cv2.imencode('.jpg', frame)
-                img_bytes = img_encoded.tobytes()
-                img_size = len(img_bytes)
-
-                # 이미지 크기를 네트워크 바이트 오더로 변환하여 전송
-                self._socket.sendall(struct.pack(">L", img_size) + img_bytes)
-        except Exception as e:
-            traceback.print_exc()
-            self._running = False
-    
-    def stop(self):
-        self._running = False
-
-
-class MessageReceiver(threading.Thread):
-    """
-    서버로부터 메시지를 수신하는 쓰레드
-
-    Args:
-        message_socket (socket.socket): 메시지 수신용 소켓
-    """
-    def __init__(self, message_socket):
-        super().__init__()
-        self._socket = message_socket           # 메시지 수신용 소켓
-        self._running = True                    # 쓰레드 실행 여부
-        self._on_message_received = self._default_callback  # 메시지 수신 시 호출할 콜백 함수
-    
-    def __del__(self):
-        self._socket.close()
-    
-    @property
-    def on_message_received(self):
-        return self._on_message_received
-    
-    @on_message_received.setter
-    def on_message_received(self, callback):
-        self._on_message_received = callback
-
-    def _default_callback(self, message):
-        """
-        메시지 수신 시 호출할 기본 콜백 함수
-
-        Args:
-            message (str): 수신한 메시지
-        """
-        print(f'Message from the server: {message}')
-
-    def run(self):
-        try:
-            while self._running:
-                # 메시지 수신
-                message = self._socket.recv(1024).decode('utf-8')
-                if not message:
-                    break
-
-                # 수신한 메시지를 콜백 함수에 전달
-                self._on_message_received(message)   
-        except Exception as e:
-            traceback.print_exc()
-            self._running = False
-    
-    def stop(self):
-        self._running = False
+    return image_send_thread, message_receive_thread
