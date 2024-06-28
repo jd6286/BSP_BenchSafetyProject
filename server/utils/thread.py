dcb4b34b499d1d223953027e00b07ceb68f84ddd
Kwon Ohjoon <koj1997@gmail.com>
2024-06-28 15:22:49 +0900

    Client1 영상 수신 종료 조건 추가

--- server/utils/thread.py
+++ server/utils/thread.py
@@ -37,7 +37,7 @@ class ImageReceiveThread(threading.Thread):
             while self._running:
                 # 이미지 크기 수신
                 img_size_data = self._socket.recv(4)
-                if not img_size_data:
+                if not img_size_data or int.from_bytes(img_size_data, 'big') == 0:
                     break
                 img_size = struct.unpack(">L", img_size_data)[0]
 
